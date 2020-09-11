8425ffa7ec0980333d4231150a359ee7f2a69de2
Joojo Dontoh <joojodontoh@gmail.com>
2020-09-11 15:17:47 +0800

    finanlising foundations

---
+++ models/payment/payment.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const db = require('../../databases/airAsia.database');
+const mongooseSoftDelete = require('mongoose-delete');
+
+const Schema = new mongoose.Schema(
+    {
+        payment_id:String,
+        status: {
+            type: String,
+            enum: ['pending', 'completed'],
+            default: "pending"
+        },
+        method: {
+            type: String,
+            default: "CREDIT_CARD"
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        total_amount: {
+            type: Number,
+            required: true
+        },
+        payment_date: {
+            type: Date,
+            default: ""
+        },
+        credit_card: {
+            type: String,
+            default: ""
+        }
+
+    },
+    { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+);
+
+Schema.pre('save', function(next) {
+    this.payment_id = this._id; 
+    next();
+});
+
+Schema.plugin(mongooseSoftDelete, { 
+    overrideMethods: 'all'
+});
+
+module.exports = db.model('payment', Schema);
